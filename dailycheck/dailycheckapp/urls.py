31fb0fdf8031666654d6b43065f55b1eeb572f40
Ramesh7128 <ramesh7128@gmail.com>
2015-01-17 18:54:12 +0530

    login

--- dailycheck/dailycheckapp/urls.py
+++ dailycheck/dailycheckapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'dailycheck.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.index, name='index'),
+    url(r'^user/', include('oautherise.urls')),
 )
