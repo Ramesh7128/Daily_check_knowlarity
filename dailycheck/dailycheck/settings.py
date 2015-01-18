2f193dbe26f0a715118c6183da6ea74cb8c93d39
Vaibhav Mule <vaibhavmule135@gmail.com>
2015-01-18 05:55:23 +0530

    hh

--- dailycheck/dailycheck/settings.py
+++ dailycheck/dailycheck/settings.py
@@ -83,8 +83,7 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = ''
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 
 
